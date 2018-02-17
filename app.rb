7362490ce01e8194da9cf1057d2de8c50812b4e8
r-craig73 <ron.craig@comcast.net>
2018-02-16 16:33:09 -0800

    add modules in app and word_spec files to pass specs

--- app.rb
+++ app.rb
@@ -5,26 +5,26 @@ require('pry')
 
 get('/') do
   @description = "This application will let a user list and define words, one word at a time."
-  @list = Words.all()
+  @list = Word_Definition_Pair::Words.all()
   erb(:input)
 end
 
 post('/output') do
-  word_definition = Words.new(params)
+  word_definition = Word_Definition_Pair::Words.new(params)
   word_definition.save
-  @list = Words.all()
+  @list = Word_Definition_Pair::Words.all()
   # binding.pry
   erb(:output)
 end
 
 get('/output/:word') do
-  @word_definition = Words.find(params[:word])
+  @word_definition = Word_Definition_Pair::Words.find(params[:word])
   @other_definition = @word_definition.new_definition_list
   erb(:definitions)
 end
 
 post('/output/:word') do
-  @word_definition = Words.find(params[:word])
-  
+  @word_definition = Word_Definition_Pair::Words.find(params[:word])
+
   erb(:definitions)
 end
--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -4,14 +4,14 @@ require('pry')
 
   describe('.all') do
     it("list is empty") do
-      Words.clear
-      expect(Words.all()).to(eq([]))
+      Word_Definition_Pair::Words.clear
+      expect(Word_Definition_Pair::Words.all()).to(eq([]))
     end
   end
 
   describe('.save') do
     it("list will save the word") do
-      word1 = Words.new({:word=> "basin", :definition=> "a washbowl"})
+      word1 = Word_Definition_Pair::Words.new({:word=> "basin", :definition=> "a washbowl"})
       word1.save
       expect(word1.word()).to(eq("basin"))
     end
@@ -19,9 +19,9 @@ require('pry')
 
   describe('.save') do
     it("list will save mutliple words") do
-      word1 = Words.new({:word=> "basin", :definition=> "a washbowl"})
+      word1 = Word_Definition_Pair::Words.new({:word=> "basin", :definition=> "a washbowl"})
       word1.save
-      word2 = Words.new({:word=> "fragile", :definition=> "easily damaged"})
+      word2 = Word_Definition_Pair::Words.new({:word=> "fragile", :definition=> "easily damaged"})
       word1.save
       expect(word1.word()).to(eq("basin"))
       expect(word2.word()).to(eq("fragile"))
@@ -30,12 +30,12 @@ require('pry')
 
   describe('.find') do
     it("finds and item based on the word") do
-      Words.clear
-      word1 = Words.new({:word=> "basin", :definition=> "a washbowl"})
+      Word_Definition_Pair::Words.clear
+      word1 = Word_Definition_Pair::Words.new({:word=> "basin", :definition=> "a washbowl"})
       word1.save
-      word2 = Words.new({:word=> "fragile", :definition=> "easily damaged"})
+      word2 = Word_Definition_Pair::Words.new({:word=> "fragile", :definition=> "easily damaged"})
       word2.save
-      expect(Words.find("basin")).to(eq(word1))
-      expect(Words.find("fragile")).to(eq(word2))
+      expect(Word_Definition_Pair::Words.find("basin")).to(eq(word1))
+      expect(Word_Definition_Pair::Words.find("fragile")).to(eq(word2))
     end
   end
