16e0216bf1b426c13e82892816a55c126a445ebd
Ron Craig <ron.craig@comcast.net>
2018-02-21 15:37:40 -0800

    add a list of words-definitions in the integration spec, two more HTML capybara files

--- spec/words_integration_spec.rb
+++ spec/words_integration_spec.rb
@@ -4,10 +4,18 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('list of words', {:type => :feature}) do
-  it('returns the word after filling out the form, click on the word to view the word and definition') do
+  it('returns a word and defintion after filling out the word and definition form several times, click on the word to view a word and definition') do
     visit('/')
     fill_in('word', :with => 'basin')
     fill_in('definition', :with => 'a sink')
+    click_button('Add!')
+    visit('/')
+    fill_in('word', :with => 'apple')
+    fill_in('definition', :with => 'a boring fruit')
+    click_button('Add!')
+    visit('/')
+    fill_in('word', :with => 'jazz')
+    fill_in('definition', :with => 'music genre known for improvisation')
     click_button('Add!')
     click_link('basin')
     expect(page).to have_content('Definition: a sink')
